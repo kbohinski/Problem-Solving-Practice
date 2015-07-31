38ddc3e88ef36146db2869d15adc8a7d0ef88b1c
Kevin Bohinski <kbohinski@gmail.com>
2015-07-31 00:44:43 -0400

    doing another codeeval

---
+++ codeEval/Binary.java
@@ -0,0 +1,13 @@
+/**
+ * @author Kevin Bohinski <bohinsk1@tcnj.edu>
+ * @version 1.0
+ * @since 2015-7-30
+ *  
+ * Problem-Solving-Practice/codeEval
+ * Binary.java
+ * Copyright (c) 2015 Kevin Bohinski. All rights reserved.
+ */
+
+/* Setting Imports */
+import java.io.*;
+
