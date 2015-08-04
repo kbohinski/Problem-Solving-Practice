f7819ed381106d070a20d73c8beb5447d8ca34e8
Kevin Bohinski <kbohinski@gmail.com>
2015-08-04 12:23:45 -0400

    did longestlines codeeval

---
+++ codeEval/LongestLines.java
@@ -0,0 +1,11 @@
+/**
+ * @author Kevin Bohinski <bohinsk1@tcnj.edu>
+ * @version 1.0
+ * @since 2015-8-4
+ *  
+ * Problem-Solving-Practice/codeEval
+ * LongestLines.java
+ * Copyright (c) 2015 Kevin Bohinski. All rights reserved.
+ */
+
+/* Setting Imports */
